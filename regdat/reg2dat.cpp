13b1c1c804771fe692553db64e6150a36f77ecba
Tony Su <peitaosu@163.com>
2019-10-08 17:46:51 +0800

    add reg file parse logic

--- regdat/reg2dat.cpp
+++ regdat/reg2dat.cpp
@@ -16,9 +16,25 @@ int Reg2Dat(std::string in_reg_path, std::string out_dat_path)
         }
         in_file.close();
     }
-    //TODO: parse reg lines and create registry hive
     ORHKEY off_hive;
     ORCreateHive(&off_hive);
+    for (std::list<wstring>::iterator it = reg_lines.begin(); it != reg_lines.end(); ++it) {
+        if (it.at(0) == '['){
+            //TODO: parse key
+            continue;
+        }
+        if (it.at(0) == '@'){
+            //TODO: parse default value
+            continue;
+        }
+        if (it.at(0) == '"'){
+            //TODO: parse value
+        }
+        if (it.at(0) == ' ')
+            continue;
+        if (it.find("Windows Registry Editor Version 5.00") == 0)
+            continue;
+    }
     std::wstring out_dat_path_w(out_dat_path.begin(), out_dat_path.end());
     ORSaveHive(off_hive, out_dat_path_w.c_str(), REG_VER_MAJOR, REG_VER_MINOR);
     return 0;
