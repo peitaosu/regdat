2515bbd8d9b8cc027a74d3d6dbfa13fc8b92b491
Tony Su <peitaosu@163.com>
2019-10-14 21:29:54 +0800

    add string wstring convert functions

--- regdat/common.cpp
+++ regdat/common.cpp
@@ -4,3 +4,13 @@ bool file_exists(std::string file_path)
 {
     return std::experimental::filesystem::exists(file_path.c_str());
 }
+
+std::wstring stringTowstring(std::string str) {
+    std::wstring wstr(str.begin(), str.end());
+    return wstr;
+}
+
+std::string wstringTostring(std::wstring wstr) {
+    std::string str(wstr.begin(), wstr.end());
+    return str;
+}
--- regdat/common.h
+++ regdat/common.h
@@ -3,3 +3,5 @@
 #include <filesystem>
 
 bool file_exists(std::string file_path);
+std::wstring stringTowstring(std::string str);
+std::string wstringTostring(std::wstring wstr);
