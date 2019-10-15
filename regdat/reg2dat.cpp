c9bec17327f646c0c1ad1ebdc310d7ee64b48511
Tony Su <peitaosu@163.com>
2019-10-15 21:54:26 +0800

    shorten the condition

--- regdat/reg2dat.cpp
+++ regdat/reg2dat.cpp
@@ -22,13 +22,14 @@ int Reg2Dat(std::string in_reg_path, std::string out_dat_path)
     ORHKEY off_hive;
     ORCreateHive(&off_hive);
     for (std::list<std::string>::iterator it = reg_lines.begin(); it != reg_lines.end(); ++it) {
-        if (it->length() == 0) {
+        if (it->length() == 0 || (it->at(0) != '@' && it->at(0) != '[' && it->at(0) != '"')) {
             continue;
         }
         if (it->at(0) == '['){
             std::string key_string = it->substr(1, it->length() - 2);
-            PORHKEY result;
-            ORCreateKey(off_hive, key_string.c_str(), NULL, NULL, NULL, result, NULL);
+            std::wstring key_wstring = stringTowstring(key_string);
+            ORHKEY result;
+            ORCreateKey(off_hive, key_wstring.c_str(), NULL, NULL, NULL, &result, NULL);
             continue;
         }
         if (it->at(0) == '@'){
@@ -38,10 +39,6 @@ int Reg2Dat(std::string in_reg_path, std::string out_dat_path)
         if (it->at(0) == '"'){
             //TODO: parse value
         }
-        if (it->at(0) == ' ')
-            continue;
-        if (it->find("Windows Registry Editor Version 5.00") == 0)
-            continue;
     }
     std::wstring out_dat_path_w(out_dat_path.begin(), out_dat_path.end());
     ORSaveHive(off_hive, out_dat_path_w.c_str(), REG_VER_MAJOR, REG_VER_MINOR);
