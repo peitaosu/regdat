de7a0e325acee2644fc8fa012e22e1896c90048c
Tony Su <peitaosu@163.com>
2019-11-07 21:04:37 +0800

    more macro

--- regdat/regdat.cpp
+++ regdat/regdat.cpp
@@ -12,16 +12,16 @@ int main(int argc, char **argv)
     Options opts(argc, argv);
     if (opts.check("--help") || opts.check("-h")) {
         std::wcout << "Usage:\n\tregdat.exe --in_reg <path_to_reg_file> --out_dat <path_to_dat_file>\n\tregdat.exe -h\\--help" << std::endl;
-        return 0;
+        return SUCCEED;
     }
     if (opts.check("--reg2dat")) {
         if (!opts.check("--in_reg")) {
             std::wcout << "--in_reg is required." << std::endl;
-            return -1;
+            return ERROR_PARAMETER_MISSED;
         }
         if (!opts.check("--out_dat")) {
             std::wcout << "--out_dat is required." << std::endl;
-            return -1;
+            return ERROR_PARAMETER_MISSED;
         }
         return Reg2Dat(opts.get_value("--in_reg"), opts.get_value("--out_dat"));
     }
